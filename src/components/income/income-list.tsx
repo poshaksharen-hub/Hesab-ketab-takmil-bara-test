05e30a4c0b821732eb1fa0ff05921c96c26dc26a
Ali Kakai <poshaksharen@gmail.com>
2025-11-20 14:35:10 +0000

    Try fixing this error: `Runtime Error: Error: AlertDialogTrigger is not

--- src/components/income/income-list.tsx
+++ src/components/income/income-list.tsx
@@ -17,6 +17,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog"
 import { USER_DETAILS } from '@/lib/constants';
 
