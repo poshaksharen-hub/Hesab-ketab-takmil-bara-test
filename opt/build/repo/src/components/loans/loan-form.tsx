fdc32808f33772b8ea19e676723a731957a0b6d4
Ali Kakai <poshaksharen@gmail.com>
2025-11-28 11:11:29 +0000

    ببین اول اینا بدون که من پروژه را دستی دانلود کردم و تو اینجا هر فایل یا

--- opt/build/repo/src/components/loans/loan-form.tsx
+++ opt/build/repo/src/components/loans/loan-form.tsx
@@ -125,6 +125,8 @@ export function LoanForm({ onCancel, onSubmit, initialData, bankAccounts, payees
         onSubmit(submissionData);
     }, [onSubmit]);
 
+    const sortedBankAccounts = [...bankAccounts].sort((a, b) => b.balance - a.balance);
+
     return (
         <Card>
         <CardHeader>
@@ -219,7 +221,7 @@ export function LoanForm({ onCancel, onSubmit, initialData, bankAccounts, payees
                             <FormItem>
                                 <FormLabel>مبلغ پیشنهادی هر قسط (تومان)</FormLabel>
                                 <FormControl>
-                                <CurrencyInput value={field.value} onChange={field.onChange} />
+                                <CurrencyInput value={field.value || 0} onChange={field.onChange} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -232,7 +234,7 @@ export function LoanForm({ onCancel, onSubmit, initialData, bankAccounts, payees
                             <FormItem>
                                 <FormLabel>تعداد پیشنهادی اقساط</FormLabel>
                                 <FormControl>
-                                <Input type="number" {...field} />
+                                <Input type="number" {...field} value={field.value || 0} />
                                 </FormControl>
                                 <FormMessage />
                             </FormItem>
@@ -248,7 +250,7 @@ export function LoanForm({ onCancel, onSubmit, initialData, bankAccounts, payees
                         <FormItem>
                             <FormLabel>روز یادآوری پرداخت در ماه</FormLabel>
                             <FormControl>
-                            <Input type="number" min="1" max="30" {...field} />
+                            <Input type="number" min="1" max="30" {...field} value={field.value || 1} />
                             </FormControl>
                             <FormDescription>
                             روز پرداخت قسط در هر ماه (مثلا: پنجم هر ماه)
@@ -305,7 +307,7 @@ export function LoanForm({ onCancel, onSubmit, initialData, bankAccounts, payees
                                         </SelectTrigger>
                                     </FormControl>
                                     <SelectContent>
-                                        {bankAccounts.map((account) => (
+                                        {sortedBankAccounts.map((account) => (
                                         <SelectItem key={account.id} value={account.id}>
                                             {`${account.bankName} ${getOwnerName(account)}`}
                                         </SelectItem>
