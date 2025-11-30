bca1c498eb4cc3f0054dd2a73d3cbd64f73223c5
Ali Kakai <poshaksharen@gmail.com>
2025-11-30 14:30:44 +0000

    بله کامل دقیقو حرفه ای با دقت پیاده سازی کن

--- src/components/transfers/transfer-form.tsx
+++ src/components/transfers/transfer-form.tsx
@@ -142,7 +142,7 @@ export function TransferForm({ onSubmit, bankAccounts, user }: TransferFormProps
                       <SelectContent className="max-h-[250px]">
                         {availableToAccounts.map((account) => (
                           <SelectItem key={account.id} value={account.id}>
-                            {`${account.bankName} ${getOwnerName(account)} - (موجودی: ${formatCurrency(account.balance, 'IRT')})`}
+                            {`${account.bankName} ${getOwnerName(account)} - (قابل استفاده: ${formatCurrency(account.balance - (account.blockedBalance || 0), 'IRT')})`}
                           </SelectItem>
                         ))}
                       </SelectContent>
