3eb684e4b23d94b338b90541fbbfd838206b4e43
Ali Kakai <poshaksharen@gmail.com>
2025-12-08 16:05:22 +0000

    Try fixing this error: `Runtime Error: Error: setIsOpen is not defined.

--- src/app/transactions/page.tsx
+++ src/app/transactions/page.tsx
@@ -160,7 +160,7 @@ export default function ExpensesPage() {
 
        <ExpenseForm
           isOpen={isFormOpen}
-          setIsOpen={setIsOpen}
+          setIsOpen={setIsFormOpen}
           onSubmit={handleFormSubmit}
           initialData={null}
           bankAccounts={allBankAccounts || []}
