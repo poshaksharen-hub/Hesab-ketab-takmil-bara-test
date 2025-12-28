e9ddddf04c15887ce4e866d1d3f39b92530346e3
Ali Kakai <poshaksharen@gmail.com>
2025-12-28 16:46:58 +0000

    revert: Remove custom Jest transform

--- jest.config.mjs
+++ jest.config.mjs
@@ -15,9 +15,6 @@ const config = {
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
-  transform: {
-    '^.+\\.(t|j)sx?$': '@swc/jest',
-  },
 }
  
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
