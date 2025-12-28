004bc063d860e40cd54f4cb4c165f7eccfc19c99
Ali Kakai <poshaksharen@gmail.com>
2025-12-28 12:17:17 +0000

    fix: Correct Jest configuration for TypeScript

--- jest.config.mjs
+++ jest.config.mjs
@@ -12,7 +12,6 @@ const config = {
   // Add more setup options before each test is run
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
   testEnvironment: 'jest-environment-jsdom',
-  preset: 'ts-jest',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
   },
